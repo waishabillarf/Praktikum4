4c0a864a53c530de4a173fb6e7eb4c4fd0b98c5b
waishabillarf <151451509+waishabillarf@users.noreply.github.com>
2024-04-30 23:49:33 +0700

    Add files via upload

---
+++ EmployeeManagement/routes/web.php
@@ -0,0 +1,15 @@
+<?php
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\EmployeeController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('home', [HomeController::class, 'index'])->name('home');
+
+Route::get('profile', ProfileController::class)->name('profile');
+
+Route::resource('employees', EmployeeController::class);
